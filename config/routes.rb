a01ad0fa404d9e6de5be6f747250aca346500d39
Steven <steventat@csus.edu>
2020-01-19 01:08:10 -0800

    created chats controller nad switched welcome view to chats

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :articles
-
-  root 'welcome#index'
+  root to: 'chats#show'
 end
